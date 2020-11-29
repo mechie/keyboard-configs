cdce8323f658e3e87a9c1e0b09ca86a857ee1c40
Guillem Martinez Salvador <gm@sal.cat>
2020-11-28 19:20:26 -0500

    Smooth out mouse movement some.

--- Preonic/mechie-workman/config.h
+++ Preonic/mechie-workman/config.h
@@ -9,3 +9,8 @@
 #define MUSIC_MASK (keycode != KC_NO)
 
 #define MIDI_BASIC
+
+#define MOUSEKEY_DELAY 300
+#define MOUSEKEY_INTERVAL 50
+#define MOUSEKEY_MAX_SPEED 8
+#define MOUSEKEY_TIME_TO_MAX 20
